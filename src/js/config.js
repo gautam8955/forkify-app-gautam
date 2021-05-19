b5f9f3e4179cc6f4a41370566135f9a07a203216
gautam8955 <gautamsaini455@gmail.com>
2021-05-19 16:22:09 +0000

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4d73cb63-4bae-46d8-b343-e2c3f34093d4';
+export const MODAL_CLOSE_SEC = 2.5;
